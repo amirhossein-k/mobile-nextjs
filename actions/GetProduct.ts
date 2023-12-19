47d4a2862295bd2c3e26999bcc967c84d7458a5e
amirhossein-k <amirhossein.k787878@gmail.com>
2023-12-19 10:41:26 +0330

    dw

--- actions/GetProduct.ts
+++ actions/GetProduct.ts
@@ -11,15 +11,24 @@ interface getproduct {
 }
 export const GetProduct = async () => {
   try {
-    const options: AxiosRequestConfig = {
-      method: "GET",
-      url: "http://localhost:3000/api/product/qhab/1",
+    const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
+    const {data} = await axios.get(
+      "http://localhost:3000/api/product/qhab/1",
+      config
+    );
+    // const options: AxiosRequestConfig = {
+    //   method: "GET",
+    //   url: "http://localhost:3000/api/product/qhab/1",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // };
     console.log("objectgggggggggg");
-    const data = await axios(options);
+    // const data = await axios(options);
     console.log(data, "yu");
     return data;
   } catch (error: any) {
