f3929f84e1a48e7dfcc70e9a2bd093db47ec9387
amirhossein-k <amirhossein.k787878@gmail.com>
2024-05-17 20:46:30 +0330

    88

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-import getUser from '../../../actions/GetUser';
-import { USER } from '../../../types';
-import SidebarDash from '@/components/dashboardstyle/Sidebar';
+import getUser from "../../../actions/GetUser";
+import { USER } from "../../../types";
+import SidebarDash from "@/components/dashboardstyle/Sidebar";
 interface ResGetUserDetail {
   message: string;
   success: boolean;
   data: any;
 }
 type DashboardLayoutProps = {
-    children: React.ReactNode;
-  };
-const page = async({ children }: DashboardLayoutProps) => {
-    const user:USER = await getUser();
-    console.log(user)
-  return (
-    <div>
+  children: React.ReactNode;
+};
+const page = async () =>
+  // {children }: DashboardLayoutProps
+  {
+    const user: USER = await getUser();
+    console.log(user);
+    return (
+      <div>
         dahsboard
-         {/* <SidebarDash user={user}>  {children}</SidebarDash> */}
-      {/* <Link href={'/dashboard/list'} >da</Link> */}
-    </div>
-  )
-}
+        {/* <SidebarDash user={user}>  {children}</SidebarDash> */}
+        {/* <Link href={'/dashboard/list'} >da</Link> */}
+      </div>
+    );
+  };
 
-export default page
+export default page;
